834295e8bd129354e9f8d71a48060fe30d666479
gosse123 <gossenahounou@gmail.com>
2026-06-21 15:35:18 +0000

    connected database

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+
 
 <template>
   <h1>bonjour</h1>
+  <checkbox label="bonjour"
+  @check="console.log('couche')"
+  @uncheck="console.log('decoucjer')"/>
 </template>
+
+<script setup>
+import checkbox  from './components/checkbox.vue';
+</script>
 
 <style scoped></style>
---
+++ frontend/src/components/checkbox.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="checkbox" @change="onChange">
+    {{ label }}
+</template>
+
+<script setup>
+defineProps({
+    label:String
+})
+const emits = defineEmits(["check","uncheck"])
+const onChange = (event)=>{
+    if(event.currentTarget.checked){
+        emits('check',currentTarget)
+    }else{
+        emits('uncheck',currentTarget)
+    }
+}
+</script>
